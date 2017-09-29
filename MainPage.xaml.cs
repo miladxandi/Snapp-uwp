a52b6b443c1d6d7f5bd6983aa0e5c9d41b4466ee
Milad Xandi <info@miladzandi.ir>
2017-09-29 07:27:36 +0330

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Controls;
+using Windows.Networking.Connectivity;
+using Windows.UI.Popups;
+
+namespace SnappApp.Views
+{
+    public sealed partial class MainPage : Page, INotifyPropertyChanged
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void Set<T>(ref T storage, T value, [CallerMemberName]string propertyName = null)
+        {
+            if (Equals(storage, value))
+            {
+                return;
+            }
+
+            storage = value;
+            OnPropertyChanged(propertyName);
+        }
+
+        private void OnPropertyChanged(string propertyName) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        private void Page_Loaded(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            var connection = NetworkInformation.GetInternetConnectionProfile();
+            bool status = (connection != null && connection.GetNetworkConnectivityLevel() == NetworkConnectivityLevel.InternetAccess);
+            if (status == true)
+            {
+                cnAsync();
+                WebViewPage wp = new WebViewPage();
+                Frame.Navigate(typeof(WebViewPage));
+            }
+            else
+            {
+                MessageDialog msgbox = new MessageDialog("خطای اتصال به اینترنت");
+            }
+
+        }
+
+        private void Button_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            var connection = NetworkInformation.GetInternetConnectionProfile();
+            bool status = (connection != null && connection.GetNetworkConnectivityLevel() == NetworkConnectivityLevel.InternetAccess);
+            if (status == true)
+            {
+                cnAsync();
+                WebViewPage wp = new WebViewPage();
+                Frame.Navigate(typeof(WebViewPage));
+            }
+            else
+            {
+                MessageDialog msgbox = new MessageDialog("خطای اتصال به اینترنت");
+            }
+        }
+        private void webView_PermissionRequested(WebView sender, WebViewPermissionRequestedEventArgs args)
+        {
+            if (args.PermissionRequest.PermissionType == WebViewPermissionType.Geolocation)
+                args.PermissionRequest.Allow();
+        }
+        public async void cnAsync()
+        {
+                var locator = new Windows.Devices.Geolocation.Geolocator();
+                var location = await locator.GetGeopositionAsync();
+                var position = location.Coordinate.Point.Position;
+            var uriMe = new Uri(@"ms-settings:privacy-location");
+        }
+    }
+}
---
+++ WebViewPage.xaml.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace SnappApp.Views
+{
+    public sealed partial class WebViewPage : Page, INotifyPropertyChanged
+    {
+        // TODO WTS: Set your hyperlink default here
+        private const string DefaultUrl = "https://app.snapp.ir";
+        private const string myUrl = "https://miladzandi.ir";
+
+        private Uri _source;
+        private Uri _my;
+
+        public Uri My
+        {
+            get { return _my; }
+            set { Set(ref _my, value); }
+        }
+        public Uri Source
+        {
+            get { return _source; }
+            set { Set(ref _source, value); }
+        }
+
+        private bool _isLoading;
+
+        public bool IsLoading
+        {
+            get
+            {
+                return _isLoading;
+            }
+
+            set
+            {
+                if (value)
+                {
+                    IsShowingFailedMessage = false;
+                }
+
+                Set(ref _isLoading, value);
+                IsLoadingVisibility = value ? Visibility.Visible : Visibility.Collapsed;
+            }
+        }
+
+        private Visibility _isLoadingVisibility;
+
+        public Visibility IsLoadingVisibility
+        {
+            get { return _isLoadingVisibility; }
+            set { Set(ref _isLoadingVisibility, value); }
+        }
+
+        private bool _isShowingFailedMessage;
+
+        public bool IsShowingFailedMessage
+        {
+            get
+            {
+                return _isShowingFailedMessage;
+            }
+
+            set
+            {
+                if (value)
+                {
+                    IsLoading = false;
+                }
+
+                Set(ref _isShowingFailedMessage, value);
+                FailedMesageVisibility = value ? Visibility.Visible : Visibility.Collapsed;
+            }
+        }
+
+        private Visibility _failedMesageVisibility;
+
+        public Visibility FailedMesageVisibility
+        {
+            get { return _failedMesageVisibility; }
+            set { Set(ref _failedMesageVisibility, value); }
+        }
+
+        private void OnNavigationCompleted(WebView sender, WebViewNavigationCompletedEventArgs args)
+        {
+            IsLoading = false;
+            OnPropertyChanged(nameof(IsBackEnabled));
+            OnPropertyChanged(nameof(IsForwardEnabled));
+        }
+
+        private void OnNavigationFailed(object sender, WebViewNavigationFailedEventArgs e)
+        {
+            // Use `e.WebErrorStatus` to vary the displayed message based on the error reason
+            IsShowingFailedMessage = true;
+        }
+
+        private void OnRetry(object sender, RoutedEventArgs e)
+        {
+            IsShowingFailedMessage = false;
+            IsLoading = true;
+
+            webView.Refresh();
+        }
+
+        public bool IsBackEnabled
+        {
+            get { return webView.CanGoBack; }
+        }
+
+        public bool IsForwardEnabled
+        {
+            get { return webView.CanGoForward; }
+        }
+
+        private void OnGoBack(object sender, RoutedEventArgs e)
+        {
+            webView.GoBack();
+        }
+
+        private void OnGoForward(object sender, RoutedEventArgs e)
+        {
+            webView.GoForward();
+        }
+
+        private void OnRefresh(object sender, RoutedEventArgs e)
+        {
+            webView.Refresh();
+        }
+
+        private async void OnOpenInBrowser(object sender, RoutedEventArgs e)
+        {
+            var uriMe = new Uri(@"ms-settings:privacy-location");
+            await Windows.System.Launcher.LaunchUriAsync(uriMe);
+        }
+
+        public WebViewPage()
+        {
+            My = new Uri(myUrl);
+            Source = new Uri(DefaultUrl);
+            InitializeComponent();
+            IsLoading = true;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void Set<T>(ref T storage, T value, [CallerMemberName]string propertyName = null)
+        {
+            if (Equals(storage, value))
+            {
+                return;
+            }
+
+            storage = value;
+            OnPropertyChanged(propertyName);
+        }
+
+        private void OnPropertyChanged(string propertyName) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
